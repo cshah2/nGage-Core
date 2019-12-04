47ea603505f355fad04f27fa7d3efdec2a1b7c9d
cshah <cshah@SA-CSHAH-VM2.bpt.com>
2019-12-04 07:38:55 -0500

    Added ORCL profile to the project

--- Test Suites/Temp.ts
+++ Test Suites/Temp.ts
@@ -11,27 +11,51 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>230b2853-26c8-4ef9-b857-25aaa1522450</testSuiteGuid>
    <testCaseLink>
-      <guid>3d9dc6fd-349d-4e50-87e1-f88921880588</guid>
+      <guid>99c2398c-3296-492d-a3f2-477a4953b898</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/P1 Regression Suite/WMI/ReferenceObjectFeature/WMI_RO017 (28604)</testCaseId>
+      <testCaseId>Test Cases/P1 Regression Suite/WMI/Segment Template/WMIST001(28646)</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>98a18471-3c8d-48ad-91ad-9f18207fdddd</guid>
+      <guid>13c296cb-e817-4c81-8d07-4b624f6cc25a</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/P1 Regression Suite/WMI/ReferenceObjectFeature/WMI_RO018 (28605)</testCaseId>
+      <testCaseId>Test Cases/P1 Regression Suite/WMI/Segment Template/WMIST002(28647)</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>9b25598a-9880-42d4-bbc2-26d6fa8ad05a</guid>
+      <guid>7ed23184-e155-4e4b-ab8d-b1093b764931</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/P1 Regression Suite/WMI/ReferenceObjectFeature/WMI_RO019 (28607)</testCaseId>
+      <testCaseId>Test Cases/P1 Regression Suite/WMI/Segment Template/WMIST003(28648)</testCaseId>
    </testCaseLink>
    <testCaseLink>
-      <guid>30db748f-6b71-4334-a217-69c03ef33f64</guid>
+      <guid>ada99960-e923-4537-b4c5-37a3446a4bc8</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/P1 Regression Suite/WMI/ReferenceObjectFeature/WMI_RO020 (28609)</testCaseId>
+      <testCaseId>Test Cases/P1 Regression Suite/WMI/Segment Template/WMIST004(28650)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7ba00bca-f4f8-4e1d-8bf5-53c471742688</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/P1 Regression Suite/WMI/Segment Template/WMIST005(28651)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>a414ea47-163a-4126-a12c-26dc7f69c418</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/P1 Regression Suite/WMI/Segment Template/WMIST006(28652)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>28910e8c-cca3-4434-a558-4514d387e59b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/P1 Regression Suite/WMI/Segment Template/WMIST007(28653)</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>fae80826-cb38-43fc-a1c3-2c7a4bec661b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/P1 Regression Suite/WMI/Segment Template/WMIST008(28654)</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
